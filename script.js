35ad16020ab6b92602b3a107a9b84319936e4728
LeotheREE <caccamleonardo@gmail.com>
2025-03-31 23:09:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1771,8 +1771,378 @@ const categories = {
         "choices": ["Infinity", "Few mega ohms", "Few kilos ohm", "Zero"],
         "answer": "Zero"
     }
+	    
+],
+
+    "ESAS REFRESHER 3": [
+      {
+        "question": "If the mass on a body were doubled, its acceleration due to gravity would be",
+        "choices": ["doubled", "haived", "unchanged", "four times"],
+        "answer": "unchanged"
+    },
+    {
+        "question": "Which of the following anchor is designed for medium and heavy weight fastening in solid materials?",
+        "choices": ["Anchor clamp", "Toggle bolt anchor", "wall grip anchor", "stud-bolt anchor"],
+        "answer": "stud-bolt anchor"
+    },
+    {
+        "question": "Under which type of loading does fatigue occur?",
+        "choices": ["Static load", "Plane load", "Repeated load", "Higher load"],
+        "answer": "Repeated load"
+    },
+    {
+        "question": "The ratio of the moment of inertia of the cross-section of the beam to the section modulus.",
+        "choices": ["Equal to the radius gyration", "Equal to the area of the cross-section", "Measure a distance", "Dependent on the modulus of elasticity of the beam measure"],
+        "answer": "Measure a distance"
+    },
+    {
+        "question": "Unit of Strain is",
+        "choices": ["meter", "milimeter", "MPa", "unitless"],
+        "answer": "unitless"
+    },
+    {
+        "question": "Poisson's ratio is the ratio of the",
+        "choices": [
+            "Unit lateral deformation to the unit longitudinal deformation",
+            "Unit stress to unit strain",
+            "Elastic limit to proportional limit",
+            "Shear strain to compressive strain"
+        ],
+        "answer": "Unit lateral deformation to the unit longitudinal deformation"
+    },
+    {
+        "question": "A simple beam 'L' meters long carries a concentrated load of 'P' Newtons at the midspan. What is the maximum bending moment of the beam?",
+        "choices": ["PL/4", "PL2/4", "PL/2", "PL2/6"],
+        "answer": "PL/2"
+    },
+    {
+        "question": "Which of the following are NOT units of pressure?",
+        "choices": ["Pa", "N/m²", "bars", "kg/m²"],
+        "answer": "kg/m²"
+    },
+    {
+        "question": "Device used to measure the velocity of a liquid?",
+        "choices": ["Venturi meter", "Manometer", "Bourdon gauge", "Pitot tube"],
+        "answer": "Pitot tube"
+    },
+    {
+        "question": "The following valves are used for shut-off service. Which one is NOT included?",
+        "choices": ["plug cock", "ball valve", "butterfly valve", "gate valve"],
+        "answer": "butterfly valve"
+    },
+    {
+        "question": "Under which of the following conditions is Bernoulli's equation valid?",
+        "choices": [
+            "All points evaluated must be on the same streamline",
+            "The fluid must be incompressible",
+            "The fluid must be inviscid",
+            "All of the above"
+        ],
+        "answer": "All of the above"
+    },
+    {
+        "question": "Point at which solid, liquid and vapor forms of a substance simultaneously exist.",
+        "choices": ["Critical", "Boiling", "Triple", "Flash"],
+        "answer": "Triple"
+    },
+    {
+        "question": "How does an adiabatic process compare to an isentropic process?",
+        "choices": [
+            "adiabatic: heat transfer = 0; isentropic: heat transfer ≠ 0",
+            "adiabatic: heat transfer ≠ 0; isentropic: heat transfer = 0",
+            "adiabatic: reversible; isentropic: not reversible",
+            "both: heat transfer = 0; isentropic: reversible"
+        ],
+        "answer": "both: heat transfer = 0; isentropic: reversible"
+    },
+    {
+        "question": "An open system of First Law of Thermodynamics should be utilized for all the following, EXCEPT",
+        "choices": ["A turbine", "A compressor", "A piston-cylinder device with no inlet/outlet", "A nozzle"],
+        "answer": "A piston-cylinder device with no inlet/outlet"
+    },
+    {
+        "question": "What law of nature underpins the concept of balanced chemical equation?",
+        "choices": ["Gay-Lussac Law", "Faraday's Law", "Second law of thermodynamics", "First law of Thermodynamics"],
+        "answer": "First law of Thermodynamics"
+    },
+    {
+        "question": "Which of the following thermodynamic cycles is the most efficient?",
+        "choices": ["Brayton", "Rankine", "Carnot", "Combined Brayton-Rankine"],
+        "answer": "Carnot"
+    },
+    {
+        "question": "What cycle is used for cooling system for aircraft?",
+        "choices": ["Bell-Coleman", "Carnot", "Brayton", "Reverse Brayton"],
+        "answer": "Bell-Coleman"
+    },
+    {
+        "question": "What is the charge of elements that belongs to group 2A (IIA)?",
+        "choices": ["+1", "-1", "+2", "-2"],
+        "answer": "+2"
+    },
+    {
+        "question": "In electrolysis, the anions migrate to the anode. Which of the following ions migrate to the other electrode?",
+        "choices": ["Acidic ions", "Neutral ions", "Basic ions", "Cations"],
+        "answer": "Cations"
+    },
+    {
+        "question": "What is the chemical symbol for Calcium Chloride?",
+        "choices": ["CaCl3", "CaCl2", "Ca2Cl", "Ca3Cl"],
+        "answer": "CaCl2"
+    },
+    {
+        "question": "What per cent of human body is carbon?",
+        "choices": ["24%", "18%", "15%", "20%"],
+        "answer": "18%"
+    },
+    {
+        "question": "What is the percent of alcohol in a 90-proof whisky?",
+        "choices": ["40%", "45%", "90%", "10%"],
+        "answer": "45%"
+    },
+    {
+        "question": "What field of study encompasses the procurement and production of metals?",
+        "choices": ["Metallurgy", "Geology", "Material Science", "Metalgraphy"],
+        "answer": "Metallurgy"
+    },
+    {
+        "question": "How many atoms are in the unit cell of a body-centered cubic (BCC) structure?",
+        "choices": ["1", "2", "3", "4"],
+        "answer": "2"
+    },
+    {
+        "question": "What is the generic name of a class of polymer which is commercially known as nylon?",
+        "choices": ["Polyacetals", "Polyamide", "Cellulose", "Polyester"],
+        "answer": "Polyamide"
+    },
+    {
+        "question": "For completely corrosion-resistant stainless steel, what minimum percentage of chromium in the alloy is required?",
+        "choices": ["1.1%", "3.2%", "15%", "11%"],
+        "answer": "11%"
+    },
+    {
+        "question": "Which of the following materials is NOT a viscoelastic material?",
+        "choices": ["plastic", "metals", "concrete", "glass"],
+        "answer": "metals"
+    },
+    {
+        "question": "What type of materials usually has a higher index of refraction?",
+        "choices": ["Lighter materials", "Heavier materials", "less dense materials", "denser materials"],
+        "answer": "denser materials"
+    },
+    {
+        "question": "Which of the following bus has a function of directing memory and I/O devices transfer?",
+        "choices": ["Data", "Device", "Address", "Control"],
+        "answer": "Control"
+    },
+    {
+        "question": "On systems where there are multiple operating systems, the decision to load a particular one is done by?",
+        "choices": ["File control block", "Process control block", "boot loader", "bootstrap"],
+        "answer": "boot loader"
+    },
+    {
+        "question": "What is the standardized communication technique for expressing instructions to a computer?",
+        "choices": ["flow chart", "programming language", "algorithm", "logic"],
+        "answer": "programming language"
+    },
+    {
+        "question": "Is an act of sending e-mail or creating web pages that are designed to collect an individual's online bank, credit card or other login information?",
+        "choices": ["Phishing", "Spam", "Hacking", "Malware"],
+        "answer": "Phishing"
+    },
+    {
+        "question": "What do you call a person who gains illegal access to a computer system?",
+        "choices": ["Computer enthusiasts", "Electronic enthusiasts", "Hacker", "Computer pirate"],
+        "answer": "Hacker"
+    },
+    {
+        "question": "Which of the following computer program is NOT considered open-source software?",
+        "choices": ["Firefox", "Microsoft office", "Linux", "OpenOffice.org"],
+        "answer": "Microsoft office"
+    },
+    {
+        "question": "A technician has installed a new video card and the computer will now only boot into VGA mode. Which of the following may be the cause of the problem?",
+        "choices": ["There is an operating system incompatibility.", "The external monitor is not compatible", "The new card is not DVI compatible", "The incorrect drivers are installed"],
+        "answer": "The incorrect drivers are installed"
+    },
+    {
+        "question": "When installing the heatsink back into a computer after replacing a failed processor, which of the following must be done during installation?",
+        "choices": ["Thermal compound must be installed between processor and heatsink.", "Thermal compound must be installed between processor and system board", "An isopropyl alcohol pad must be placed between the processor and the heatsink", "An isopropyl alcohol pad must be placed between the processor and the system board."],
+        "answer": "Thermal compound must be installed between processor and heatsink."
+    },
+    {
+        "question": "Matthew is downloading an 800 kB file from the internet using his 28.8 k modem. How long will it take him to finish downloading?",
+        "choices": ["0.46 min", "3.7 min", "3.8 min", "4.1 min"],
+        "answer": "3.8 min"
+    },
+    {
+        "question": "In a spreadsheet, the formula $A$3+$B3+D2 is entered into cell C2. The contents of cell C2 are copied and pasted into cell D5. The formula in cell D5 is?",
+        "choices": ["$A$3+CS2+C4", "$B$6+SC4+C4", "$A$3+$B6+E5", "$A$3+$B2+B2"],
+        "answer": "$A$3+$B6+E5"
+    },
+    {
+        "question": "Which function is NOT provided by the MATLAB Mathematical function library?",
+        "choices": ["Bessel functions", "Matrix eigenvalues", "Fourier transforms", "Line diagram"],
+        "answer": "Line diagram"
+    },
+    {
+        "question": "The MATLAB command will correctly define x, y, and z as symbols?",
+        "choices": ["syms (x,y,z)", "syms xyz", "sym (x, y,z)", "sym x,y,z"],
+        "answer": "syms xyz"
+    },
+{
+        "question": "MATLAB command for getting the roots of 3x^2-4x+7=0",
+        "choices": ["Roots", "Poly", "Polyfit", "Plot"],
+        "answer": "Roots"
+    },
+    {
+        "question": "By default, all the files in C++ are opened in ___ mode.",
+        "choices": ["Binary", "VTC", "ISCII", "Text"],
+        "answer": "Text"
+    },
+    {
+        "question": "In financial accounting another term for Acid Test Ratio is",
+        "choices": ["Quick ratio", "Current ratio", "Profit margin ratio", "Price-earning ratio"],
+        "answer": "Quick ratio"
+    },
+    {
+        "question": "Future worth is equivalent to _",
+        "choices": ["P(1+n)Nj", "Pi1+nj^(n-1)", "P(1+i)^n", "Pi1+i)^(-n)"],
+        "answer": "P(1+i)^n"
+    },
+    {
+        "question": "Which of the following expressions is INCORRECT?",
+        "choices": [
+            "The future worth of a present amount, (F/P, i, n)= 1/(P/F, i,n)",
+            "The future worth of an annuity, (F/A,i, n)= 1/(AAF,i, n)",
+            "The present worth of an annuity,(P/A,i n) = iA/P, i, n)",
+            "(ArF i, n)-i=(AP,i, n)"
+        ],
+        "answer": "(ArF i, n)-i=(AP,i, n)"
+    },
+    {
+        "question": "The amount of property in which a willing buyer will pay to a willing seller for a property when neither one is under the compulsion to buy nor sell is called _ value.",
+        "choices": ["Goodwill", "Second-hand", "Market", "Fair"],
+        "answer": "Market"
+    },
+    {
+        "question": "What refers to the establishing interrelationships between people and things in such a way that human and material resources are effectively focused toward achieving the goal of the company?",
+        "choices": ["Planning", "Leading", "Organizing", "Controlling"],
+        "answer": "Organizing"
+    },
+    {
+        "question": "Which of the following techniques must a manager use if he decides to absorb the risk in the project?",
+        "choices": [
+            "Use a different technology",
+            "Buying insurance",
+            "Use a different supplier",
+            "Create buffer in the form of management reserve or extra time to schedule"
+        ],
+        "answer": "Create buffer in the form of management reserve or extra time to schedule"
+    },
+    {
+        "question": "A bank is advertising 9.5% account that yield 9.84% annually. Find how often interest is compounded.",
+        "choices": ["Monthly", "Bimonthly", "Annually", "Quarterly"],
+        "answer": "Quarterly"
+    },
+    {
+        "question": "When was R.A. 7920 passed by the House of Representatives and the Senate?",
+        "choices": ["February 14, 1995", "February 21, 1994", "February 24, 1995", "February 21, 1995"],
+        "answer": "February 21, 1995"
+    },
+    {
+        "question": "Which is NOT a requirement for PRBEE?",
+        "choices": [
+            "At least 40 yrs old, good moral conduct",
+            "At least 10 yrs experience",
+            "Resident of the PH for 5 consecutive years",
+            "Graduated BSEE from a PH-gov't recognized institution"
+        ],
+        "answer": "At least 40 yrs old, good moral conduct"
+    },
+    {
+        "question": "Based on section 18 of R.A. 7920, the minimum age requirement for qualifications of applicants for Registered Master Electrician (RME) examination is at least _ years of age.",
+        "choices": ["18", "19", "21", "23"],
+        "answer": "18"
+    },
+    {
+        "question": "What is Rule 11 in R.A. 7920?",
+        "choices": ["Action on Communication", "Places and Dates of Examinations", "Program of Examination", "Publication of Examination"],
+        "answer": "Places and Dates of Examinations"
+    },
+    {
+        "question": "Which of the following is qualified as the chairman of the Continuing Professional Development Council for Electrical Engineering?",
+        "choices": [
+            "Representative from Association of Professional Organization (APO)",
+            "PRBEE board member",
+            "Representative from Academy",
+            "PRC Commissioner"
+        ],
+        "answer": "PRBEE board member"
+    },
+    {
+        "question": "Electrical equipment must be grounded to ensure that dangerous voltage on metal parts resulting from a ground fault can be reduced to a safe value. (I. True, II. False)",
+        "choices": ["I only", "II only", "Both I or II", "Either I or II"],
+        "answer": "I only"
+    },
+    {
+        "question": "Rod and pipe electrodes shall NOT be less than ___ mm in length.",
+        "choices": ["1,500", "2,400", "1,800", "2,800"],
+        "answer": "2,400"
+    },
+    {
+        "question": "Which of the following insulation color identifies the grounded conductor of a branch circuit?",
+        "choices": ["Black", "Yellow", "Green", "White"],
+        "answer": "White"
+    },
+    {
+        "question": "Which of the following is NOT a hazardous practice?",
+        "choices": [
+            "Using the right tools/equipment",
+            "Using improvised tools",
+            "Servicing equipment in motion",
+            "Operating without authority"
+        ],
+        "answer": "Using the right tools/equipment"
+    },
+    {
+        "question": "What is the standard color for safety signages to indicate the presence of radiation hazards, example in the X-ray laboratory in the hospital?",
+        "choices": ["Green with combination of red", "Orange", "Blue", "Purple with combination of yellow"],
+        "answer": "Purple with combination of yellow"
+    },
+    {
+        "question": "What is the Law for renewable energy?",
+        "choices": ["R.A. 9136", "R.A. 11285", "R.A. 11361", "R.A. 9513"],
+        "answer": "R.A. 9513"
+    },
+    {
+        "question": "Tall growing plants refer to plants, vines, and other plant species, which grow or have the potential to grow more than _ in height from the ground.",
+        "choices": ["2 meters", "5 meters", "1 meter", "3 meters"],
+        "answer": "3 meters"
+    },
+    {
+        "question": "The land beneath, the air spaces surrounding, and the area traversed by power lines including its horizontal, vertical, and similar clearance requirements shall constitute the ___",
+        "choices": ["Creepage distance", "Private property", "Power line corridor", "Public property"],
+        "answer": "Power line corridor"
+    },
+    {
+        "question": "What is the Law for Anti-Obstruction of powerline?",
+        "choices": ["R.A. 9136", "R.A. 11285", "R.A. 11361", "R.A. 9513"],
+        "answer": "R.A. 11361"
+    },
+    {
+        "question": "What is the Law for Energy Eff and Covservation Act?",
+        "choices": ["R.A. 9136", "R.A. 11285", "R.A. 11361", "R.A. 9513"],
+        "answer": "R.A. 11285"
+    },
+    {
+        "question": "What is the Epira Law?",
+        "choices": ["R.A. 9136", "R.A. 11285", "R.A. 11361", "R.A. 9513"],
+        "answer": "R.A. 9136"
+    }
 
 ],
+	
 };
 
 
